08b2da6a17d393e2e5cae62178fc1317ffc2ce40
Alena <astarikova@k2.cloud>
2025-06-30 22:13:24 +0300

    [26] connect backend with llm for color type recommendation

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,9 @@ load_dotenv()
 
 PREDICT_BODY_TYPE_ML_URL = "http://host.docker.internal:8000/predict"
 PREDICT_BODY_TYPE_LLM_URL = "http://host.docker.internal:8001/recommend"
+
 PREDICT_COLOR_TYPE_URL = "http://host.docker.internal:8000/predict_color_type"
+PREDICT_COLOR_TYPE_LLM_URL = "http://host.docker.internal:8001/recommend_by_color_type"
 
 DATABASE_URL="postgresql+asyncpg://styleu_user:password@db:5432/styleu"
 SECRET_KEY = os.getenv("SECRET_KEY")
--- backend/services/style_service.py
+++ backend/services/style_service.py
@@ -1,7 +1,12 @@
 from databases.database_connector import DatabaseConnector
 import httpx, tempfile, shutil, os
 from fastapi import UploadFile, HTTPException
-from config import PREDICT_BODY_TYPE_ML_URL, PREDICT_BODY_TYPE_LLM_URL, PREDICT_COLOR_TYPE_URL
+from config import (
+    PREDICT_BODY_TYPE_ML_URL,
+    PREDICT_BODY_TYPE_LLM_URL,
+    PREDICT_COLOR_TYPE_URL,
+    PREDICT_COLOR_TYPE_LLM_URL
+)
 
 async def analyze_body_type(height, bust, waist, hips, username=None):
     try:
@@ -43,16 +48,29 @@ async def analyze_color_type(file: UploadFile, username=None):
         shutil.copyfileobj(file.file, tmp)
         temp_path = tmp.name
     try:
-        async with httpx.AsyncClient() as client:
+        async with httpx.AsyncClient(timeout=30.0) as client:
             response = await client.post(PREDICT_COLOR_TYPE_URL, json={"path": temp_path})
             response.raise_for_status()
             color_type = response.json().get("color_type")
+
+            llm_response = await client.post(PREDICT_COLOR_TYPE_LLM_URL, json={"color_type": color_type})
+            print(llm_response)
+            llm_response.raise_for_status()
+            recommendation = llm_response.json()
 
         if username:
             async with DatabaseConnector() as connector:
                 user_id = await connector.get_user_id(username)
                 await connector.set_color_type(user_id, color_type)
 
-        return {"color_type": color_type}
+        return {"color_type": color_type, "recommendation": recommendation}
+
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=502, detail=f"Request error: {str(e)}")
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=f"Service error: {e.response.text}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+
     finally:
         os.remove(temp_path)
