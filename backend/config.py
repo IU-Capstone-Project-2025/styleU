97dbfcda1c782f66055bc037ad6ced3fce505f53
Maria <m.rokkel@innopolis.university>
2025-07-01 18:43:40 +0300

    frontend and backend connection

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-PREDICT_BODY_TYPE_ML_URL = "http://host.docker.internal:8000/predict"
-PREDICT_BODY_TYPE_LLM_URL = "http://host.docker.internal:8001/recommend"
+PREDICT_BODY_TYPE_ML_URL = "http://localhost:8000/predict"
+PREDICT_BODY_TYPE_LLM_URL = "http://localhost:8001/recommend"
 
 PREDICT_COLOR_TYPE_URL = "http://localhost:8000/predict_color_type"
