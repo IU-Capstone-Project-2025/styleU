ab7a6e72527edfb7e4221dcb7bcc6242ddd5be05
Aisylu Fattakhova <a.fattakhova@innopolis.university>
2025-07-08 13:24:04 +0300

    faceid_adapter_run.py - Connected to IP-Adapter-FaceID in HF space; main.py - added endpont to generate avatar;

---
+++ ml-service/app/faceid_adapter/faceid_adapter_run.py
@@ -0,0 +1,35 @@
+from gradio_client import Client, handle_file
+from gradio_client.exceptions import AppError
+from pathlib import Path
+import requests
+def generate_stylized_avatar(input_image_path: Path, output_dir: Path):
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    client = Client("multimodalart/Ip-Adapter-FaceID", hf_token="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")
+
+    try:
+        result = client.predict(
+            images=[handle_file(str(input_image_path))],
+            prompt="in bratz style",
+            negative_prompt="blurry, low quality",
+            preserve_face_structure=True,
+            face_strength=1.3,
+            likeness_strength=1,
+            nfaa_negative_prompt="naked, bikini, skimpy, scanty, bare skin, lingerie, swimsuit, exposed, see-through",
+            api_name="/generate_image"
+        )
+    except AppError as e:
+        raise RuntimeError(f"Gradio app error: {str(e)}")
+
+    # Скачивание 
+    # Это вариант скачивания фотки с локальной машины/диска
+    image_url = result[0]['image']
+    image_path = Path(image_url)  
+    with open(image_path, "rb") as f:
+        image_data = f.read()
+    result_path = output_dir / f"{input_image_path.stem}_avatar.jpg"
+
+    with open(result_path, 'wb') as f:
+        f.write(image_data)
+
+    return result_path
--- ml-service/app/main.py
+++ ml-service/app/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from app.schema import BodyParams, PredictionResult, ImagePath, PredictionColorResult
 from app.model import predict_body_type
 from app.colortype_model import get_features_from_image, predict_color_types
+from app.faceid_adapter.faceid_adapter_run import generate_stylized_avatar
+from pathlib import Path
+import shutil
 
 app = FastAPI()
 
@@ -20,3 +23,17 @@ def predict_color_type(params: ImagePath):
     features = get_features_from_image(params.path)
     color_type = predict_color_types(features)
     return {"color_type": color_type}
+
+@app.post("/generate-avatar/")
+async def generate_avatar(image: UploadFile = File(...)):
+    # Сохраняем загруженный файл во временную директорию
+    input_path = Path(f"temp_inputs/{image.filename}")
+    input_path.parent.mkdir(parents=True, exist_ok=True)
+    with input_path.open("wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    # Путь для вывода
+    output_dir = Path("generated_faces")
+    result_path = generate_stylized_avatar(input_path, output_dir)
+
+    return {"generated_image": str(result_path)}
